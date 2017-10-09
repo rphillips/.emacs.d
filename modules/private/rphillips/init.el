e33a2bddea684740fc2b3625ad60a9cc354e1dfb
Ryan Phillips <ryan.phillips@coreos.com>
2017-10-09 11:30:49 -0500

    add my tweaks

---
+++ modules/private/rphillips/init.el
@@ -0,0 +1,27 @@
+;;; private/hlissner/init.el -*- lexical-binding: t; -*-
+
+;; I've swapped these keys on my keyboard
+(setq x-super-keysym 'alt
+      x-alt-keysym   'meta
+
+      user-mail-address "ryan@trolocsis.com"
+      user-full-name    "Ryan Phillips")
+
+;; An extra measure to prevent the flash of unstyled mode-line while Emacs is
+;; booting up (when Doom is byte-compiled).
+(setq-default mode-line-format nil)
+
+(setq projectile-mode-line
+      '(:eval (format " Projectile[%s]" (projectile-project-name))))
+
+;; (setq doom-theme 'doom-molokai)
+
+(set! :font "Source Code Pro" :size 12)
+(set! :variable-font "Source Code Pro" :size 12)
+(set! :unicode-font "Source Code Pro" :size 12)
+
+;; host-specific settings
+(pcase (system-name)
+  ("Ryans-MBP" (set! :font "Source Code Pro" :size 12)
+               (set! :variable-font "Source Code Pro" :size 12)
+               (set! :unicode-font "Source Code Pro" :size 12)))
